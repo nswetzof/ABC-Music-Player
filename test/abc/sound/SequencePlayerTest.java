285425971ff38bcc64bf31c1abb4e15125a83249
Nathan Swetzof <nswetzof@gmail.com>
2021-02-15 12:13:55 -0800

    Write SequencePlayer code for piece1.abc and correct errors in piece1.abc.

--- test/abc/sound/SequencePlayerTest.java
+++ test/abc/sound/SequencePlayerTest.java
@@ -21,29 +21,43 @@ public class SequencePlayerTest {
     
     @Test
     public void test() {
-        fail("Not yet implemented");
-    }
-    
-    @Test
-    public void testPiece1() {
     	try {
-    		SequencePlayer player = new SequencePlayer(140, 4);
+    		SequencePlayer player = new SequencePlayer(140, 12);
     		
-    		player.addNote(C.toMidiNote(), 0, 4); // TODO: why do these play so slow?
-    		player.addNote(C.toMidiNote(), 4, 4);
-    		player.addNote(C.toMidiNote(), 8, 3);
-    		player.addNote(D.toMidiNote(), 11, 1);
-    		player.addNote(E.toMidiNote(), 12, 4);
+    		player.addNote(C.toMidiNote(), 0, 12);
+    		player.addNote(C.toMidiNote(), 12, 24);
+    		player.addNote(C.toMidiNote(), 24, 9);
+    		player.addNote(D.toMidiNote(), 33, 3);
+    		player.addNote(E.toMidiNote(), 36, 12);
     		
-    		player.addNote(E.toMidiNote(), 16, 3);
-    		player.addNote(D.toMidiNote(), 19, 1);
-    		player.addNote(E.toMidiNote(), 20, 3);
-    		player.addNote(F.toMidiNote(), 23, 1);
-    		player.addNote(G.toMidiNote(), 24, 8);
+    		player.addNote(E.toMidiNote(), 48, 9);
+    		player.addNote(D.toMidiNote(), 57, 3);
+    		player.addNote(E.toMidiNote(), 60, 9);
+    		player.addNote(F.toMidiNote(), 69, 3);
+    		player.addNote(G.toMidiNote(), 72, 24);
+    		
+    		player.addNote(C.transpose(Pitch.OCTAVE).toMidiNote(), 96, 4);
+    		player.addNote(C.transpose(Pitch.OCTAVE).toMidiNote(), 100, 4);
+    		player.addNote(C.transpose(Pitch.OCTAVE).toMidiNote(), 104, 4);
+    		player.addNote(G.toMidiNote(), 108, 4);
+    		player.addNote(G.toMidiNote(), 112, 4);
+    		player.addNote(G.toMidiNote(), 116, 4);
+    		player.addNote(E.toMidiNote(), 120, 4);
+    		player.addNote(E.toMidiNote(), 124, 4);
+    		player.addNote(E.toMidiNote(), 128, 4);
+    		player.addNote(C.toMidiNote(), 132, 4);
+    		player.addNote(C.toMidiNote(), 136, 4);
+    		player.addNote(C.toMidiNote(), 140, 4);
+    		
+    		player.addNote(G.toMidiNote(), 144, 9);
+    		player.addNote(F.toMidiNote(), 153, 3);
+    		player.addNote(E.toMidiNote(), 156, 9);
+    		player.addNote(D.toMidiNote(), 165, 3);
+    		player.addNote(C.toMidiNote(), 168, 24);
     		
     		//System.err.println(player.toString());
     		
-    		//player.play();
+    		player.play();
     		
     	}
     	catch(MidiUnavailableException mue) {
@@ -53,6 +67,55 @@ public class SequencePlayerTest {
     		fail(imde.getStackTrace().toString());
     	}
     }
+    
+//    @Test
+//    public void testPiece1() {
+//    	try {
+//    		SequencePlayer player = new SequencePlayer(140, 12);
+//    		
+//    		player.addNote(C.toMidiNote(), 0, 12);
+//    		player.addNote(C.toMidiNote(), 12, 24);
+//    		player.addNote(C.toMidiNote(), 24, 9);
+//    		player.addNote(D.toMidiNote(), 33, 3);
+//    		player.addNote(E.toMidiNote(), 36, 12);
+//    		
+//    		player.addNote(E.toMidiNote(), 48, 9);
+//    		player.addNote(D.toMidiNote(), 57, 3);
+//    		player.addNote(E.toMidiNote(), 60, 9);
+//    		player.addNote(F.toMidiNote(), 69, 3);
+//    		player.addNote(G.toMidiNote(), 72, 24);
+//    		
+//    		player.addNote(C.transpose(Pitch.OCTAVE).toMidiNote(), 96, 4);
+//    		player.addNote(C.transpose(Pitch.OCTAVE).toMidiNote(), 100, 4);
+//    		player.addNote(C.transpose(Pitch.OCTAVE).toMidiNote(), 104, 4);
+//    		player.addNote(G.toMidiNote(), 108, 4);
+//    		player.addNote(G.toMidiNote(), 112, 4);
+//    		player.addNote(G.toMidiNote(), 116, 4);
+//    		player.addNote(E.toMidiNote(), 120, 4);
+//    		player.addNote(E.toMidiNote(), 124, 4);
+//    		player.addNote(E.toMidiNote(), 128, 4);
+//    		player.addNote(C.toMidiNote(), 132, 4);
+//    		player.addNote(C.toMidiNote(), 136, 4);
+//    		player.addNote(C.toMidiNote(), 140, 4);
+//    		
+//    		player.addNote(G.toMidiNote(), 144, 9);
+//    		player.addNote(F.toMidiNote(), 153, 3);
+//    		player.addNote(E.toMidiNote(), 156, 9);
+//    		player.addNote(D.toMidiNote(), 165, 3);
+//    		player.addNote(C.toMidiNote(), 168, 24);
+//    		
+//    		//System.err.println(player.toString());
+//    		
+//    		player.play();
+//    		
+//    	}
+//    	catch(MidiUnavailableException mue) {
+//    		fail(mue.getStackTrace().toString());
+//    	}
+//    	catch(InvalidMidiDataException imde) {
+//    		fail(imde.getStackTrace().toString());
+//    	}
+//    }
     
 
 }
